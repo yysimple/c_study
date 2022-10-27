e32d93215582780fc5f67d3b74cbe84d32ab729c
wuchengxing <1449697757@qq.com>
2022-10-27 23:14:44 +0800

    feat：c里面的数组结构

---
+++ arr/array.c
@@ -0,0 +1,10 @@
+//
+// Created by cheng xing wu on 2022/10/27.
+//
+
+#include "array.h"
+
+void array_test() {
+    int age[10] = {1, 2, 3};
+    char name[] = {'name'};
+}
---
+++ arr/array.h
@@ -0,0 +1,8 @@
+//
+// Created by cheng xing wu on 2022/10/27.
+//
+
+#ifndef C_STUDY_ARRAY_H
+#define C_STUDY_ARRAY_H
+
+#endif //C_STUDY_ARRAY_H
