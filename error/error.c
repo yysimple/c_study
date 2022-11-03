3c778d4552584267674f652ae00abfa2f7151c49
wuchengxing <1449697757@qq.com>
2022-11-03 19:46:59 +0800

    feat：error操作

---
+++ error/error.c
@@ -0,0 +1,5 @@
+//
+// Created by cheng xing wu on 2022/11/3.
+//
+
+#include "error.h"
---
+++ error/error.h
@@ -0,0 +1,8 @@
+//
+// Created by cheng xing wu on 2022/11/3.
+//
+
+#ifndef C_STUDY_ERROR_H
+#define C_STUDY_ERROR_H
+
+#endif //C_STUDY_ERROR_H
